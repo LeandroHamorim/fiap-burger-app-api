cab3b60c53a9f169447ddec73bcaa0b33b6055df
phasmacaptain <slaviklarissa@gmail.com>
2024-03-10 18:47:13 -0300

    ajusta variaveis

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1" // Substitua pela sua região preferida
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "A região da AWS onde o bucket será criado"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "O nome do bucket S3 a ser criado"
+  type        = string
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "hello-world"
+  bucket = var.bucket_name
   acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "fiap-burger-bucket-s3"
+    Environment = "Dev"
+  }
+
+  public_access_block_configuration {
+    block_public_acls       = true
+    ignore_public_acls      = true
+    block_public_policy     = true
+    restrict_public_buckets = true
+  }
 }
