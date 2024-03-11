4e6b3207ee0bd1ebfa871c2ce20f9220c2fd1389
FiapBurger <fiapburger.tech@gmail.com>
2024-03-11 19:09:06 -0300

    feat: simplificando terraform

--- src/main.tf
+++ src/main.tf
@@ -1,53 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "A região da AWS onde o bucket será criado"
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "O nome do bucket S3 a ser criado"
-  type        = string
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name        = "fiap-burger-bucket-s3"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "my_bucket_access_block" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  block_public_acls   = true
-  ignore_public_acls  = true
-  block_public_policy = true
-  restrict_public_buckets = true
+  bucket = "hello-world"
 }
