6346900b4669b120e6ba13c429324bba26e854d4
FiapBurger <fiapburger.tech@gmail.com>
2024-03-15 21:52:37 -0300

    feat: first s3

--- main.tf
+++ main.tf
@@ -1,7 +1,34 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = "my-first-private-bucket"
+  region = "us-east"
+
+  tags = {
+    Name        = "My First Private Bucket"
+    Environment = "Exercise"
+  }
 }
 
-resource "aws_s3_bucket" "my_bucket_1" {
-  bucket = "hello-world"
+resource "aws_s3_bucket_acl" "private_bucket_acl" {
+  bucket = aws_s3_bucket.private_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-first-public-bucket"
+  region = "us-east"
+
+  tags = {
+    Name        = "My First Public Bucket"
+    Environment = "Exercise"
+  }
+
+}
+
+resource "aws_s3_bucket_acl" "public_bucket_acl" {
+  bucket = aws_s3_bucket.public_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_object" "bucket_object" {
+  content  = "object content"
 }
