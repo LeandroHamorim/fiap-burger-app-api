33448f3a4d683904c5e1872b704ec76f65a111f1
Alexandre <alexandre@Macbook-de-Leonardo.local>
2024-03-17 17:35:49 -0300

    feat : eks cluster

--- main.tf
+++ main.tf
@@ -2,7 +2,60 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_eks_cluster" "my_cluster_" {
+output "endpoint" {
+  value = aws_eks_cluster.fiap_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.fiap_cluster.certificate_authority[0].data
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "fiap_cluster" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.fiap_cluster.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.fiap_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.fiap_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.fiap_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.fiap_node_group_role.name
+}
+
+resource "aws_eks_cluster" "fiap_cluster" {
   name     = "fiap-burger-eks-cluster"
   role_arn = "arn:aws:iam::730335333567:role/LabRole"
 
@@ -12,22 +65,47 @@ resource "aws_eks_cluster" "my_cluster_" {
       "subnet-0b97e6916779e8ddb"
     ]
   }
+}
 
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+resource "aws_iam_role" "fiap_node_group_role" {
+  name = "eks-node-group-ec2-role"
 
-      instance_type = "m5.large"
-    }
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_eks_node_group" "fiap_node_group" {
+  cluster_name    = aws_eks_cluster.fiap_cluster.name
+  node_group_name = "fiap_node_group"
+  node_role_arn   = aws_iam_role.fiap_node_group_role.arn
+  subnet_ids      = [
+    "subnet-081d1613d89ee8ba6",
+    "subnet-0b97e6916779e8ddb"
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
-}
 
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
+  update_config {
+    max_unavailable = 1
+  }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
